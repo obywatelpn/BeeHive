a11c2e75e4ff2fbae1d87aa1c1ad765f5c49fdcb
NP <np@test.pl>
2016-12-13 23:18:10 +0100

    add bee class

---
+++ BeeHive/Bee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeeHive
+{
+    class Bee
+    {
+        public virtual int MyProperty { get { return 0; }}
+    }
+}
